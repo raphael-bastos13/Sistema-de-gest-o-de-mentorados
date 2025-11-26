71e2782b971ca1cbfb3bf77fad5cf89b0923fdf2
Raphael Bastos <raphael.10.bastos@gmail.com>
2025-11-26 16:45:23 -0300

    melhorias na pagina de mentorados(grafico)

--- mentorados/views.py
+++ mentorados/views.py
@@ -7,29 +7,43 @@ from django.contrib.messages import constants
 # Create your views here.
 def mentorados(request):
     if not request.user.is_authenticated:
-          return redirect('login')
+        return redirect('login')
     
     if request.method == 'GET':
-            navigators = Navigators.objects.filter(user=request.user)
-            mentorados = Mentorados.objects.filter(user=request.user)
-            return render(request, "mentorados.html", {'estagios':Mentorados.estagio_choices, 'navigators': navigators, 'mentorados': mentorados})
-            
+        navigators = Navigators.objects.filter(user=request.user)
+        mentorados = Mentorados.objects.filter(user=request.user)   
+        
+        qtd_estagio = []
+        estagio_flat = []
+        
+        for i, label in Mentorados.estagio_choices:
+            estagio_flat.append(label)
+            x = Mentorados.objects.filter(estagio=i, user=request.user).count()
+            qtd_estagio.append(x)
+
+        return render(request, "mentorados.html", {
+            'estagios': Mentorados.estagio_choices, 
+            'navigators': navigators, 
+            'mentorados': mentorados, 
+            'estagios_labels': estagio_flat,  # ← mudado aqui
+            'qtd_estagio': qtd_estagio
+        })
     
     elif request.method == 'POST':
-          nome = request.POST.get('nome')
-          foto = request.FILES.get('foto')
-          estagio = request.POST.get("estagio")
-          navigator = request.POST.get('navigator')
+        nome = request.POST.get('nome')
+        foto = request.FILES.get('foto')
+        estagio = request.POST.get("estagio")
+        navigator = request.POST.get('navigator')
 
-          mentorado = Mentorados(
-                nome=nome,
-                foto=foto,
-                estagio=estagio,
-                navigator_id=navigator,
-                user=request.user
-          )
+        mentorado = Mentorados(
+            nome=nome,
+            foto=foto,
+            estagio=estagio,
+            navigator_id=navigator,
+            user=request.user
+        )
 
-          mentorado.save()
+        mentorado.save()
 
-          messages.add_message(request, constants.SUCCESS, 'Mentorado cadastrado com sucesso.')
-          return redirect('mentorados')
+        messages.add_message(request, constants.SUCCESS, 'Mentorado cadastrado com sucesso.')
+        return redirect('mentorados')
